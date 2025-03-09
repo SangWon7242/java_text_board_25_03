39b8899a40f97884cdb997e44a67eda7522f4675
SangWon7242 <gotkdqja@gmail.com>
2025-03-09 13:06:13 +0900

    프로그램 시작 메시지 출력

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -1,7 +1,9 @@
 package com.sbs.java.text_board;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("== JAVA 텍스트 게시판 구현 ==");
+    System.out.println("프로그램 시작");
+    System.out.println("== JAVA 텍스트 게시판 종료 ==");
+  }
 }
