1ab7d52be32b1c7d303212145a7ca4ada0892efe
SangWon7242 <gotkdqja@gmail.com>
2025-03-09 13:40:47 +0900

    toString 메서드를 이용하여 객체를 설명적으로 표현

--- src/main/java/com/sbs/java/text_board/Article.java
+++ src/main/java/com/sbs/java/text_board/Article.java
@@ -4,4 +4,9 @@ public class Article {
   int id;
   String subject;
   String content;
+
+  @Override
+  public String toString() {
+    return "{id: %d, subject: \"%s\", content: \"%s\"}".formatted(id, subject, content);
+  }
 }
