24af9a455109936a6368a4503031675cc28acc58
SangWon7242 <gotkdqja@gmail.com>
2025-03-09 13:18:14 +0900

    게시물 제목, 내용 입력받기

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -12,12 +12,25 @@ public class Main {
       System.out.print("명령어) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")) {
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성 ==");
+        System.out.print("제목 : ");
+        String subject = sc.nextLine();
+
+        System.out.print("내용 : ");
+        String content = sc.nextLine();
+
+        int id = 1;
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      }
+      else if(cmd.equals("exit")) {
         System.out.println("게시판 프로그램을 종료합니다.");
         break;
       }
-
-      System.out.printf("입력받은 명령어 : %s\n", cmd);
+      else {
+        System.out.println("명령어를 잘 못 입력하였습니다.");
+      }
     }
 
     System.out.println("== JAVA 텍스트 게시판 종료 ==");
