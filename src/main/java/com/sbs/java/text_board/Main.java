e7b75a3c7aaa259ee9e9f40bf43f4d25a0a74741
SangWon7242 <gotkdqja@gmail.com>
2025-03-09 13:14:07 +0900

    exit 나올 때까지 계속 입력

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -8,10 +8,17 @@ public class Main {
 
     System.out.println("== JAVA 텍스트 게시판 구현 ==");
 
-    System.out.print("명령어) ");
-    String cmd = sc.nextLine();
+    while (true) {
+      System.out.print("명령어) ");
+      String cmd = sc.nextLine();
 
-    System.out.printf("입력받은 명령어 : %s\n", cmd);
+      if(cmd.equals("exit")) {
+        System.out.println("게시판 프로그램을 종료합니다.");
+        break;
+      }
+
+      System.out.printf("입력받은 명령어 : %s\n", cmd);
+    }
 
     System.out.println("== JAVA 텍스트 게시판 종료 ==");
     sc.close();
