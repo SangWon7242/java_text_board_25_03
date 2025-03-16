714b22473f38852cc0362ccc070419bb4d2ebbd8
SangWon7242 <gotkdqja@gmail.com>
2025-03-16 12:22:06 +0900

    입력 된 id 값에 대한 유효성 검사

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -49,7 +49,20 @@ public class Main {
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       } else if (rq.getUrlPath().equals("/usr/article/detail")) {
         Map<String, String> params = rq.getParams();
-        int id = Integer.parseInt(params.get("id"));
+
+        if(!params.containsKey("id")) {
+          System.out.println("id값을 입력해주세요.");
+          continue;
+        }
+
+        int id = 0;
+
+        try {
+          id = Integer.parseInt(params.get("id"));
+        } catch (NumberFormatException e) {
+          System.out.println("id를 정수형태로 입력해주세요.");
+          continue;
+        }
 
         // 리스트에 게시물이 비어 있는 경우
         if (articles.isEmpty()) {
